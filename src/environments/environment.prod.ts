3de06e3967c51e9f458bd05daf1a3e45a13388d1
Edwin <edwin.viljoen.1993@gmail.com>
2022-10-12 09:30:56 +0200

    added material, tailwind, pixabay env vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  key: '30538748-bac22a4d78aa0d6b1174e33dd',
+  apiUrl: `https://pixabay.com/api`
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  key: '30538748-bac22a4d78aa0d6b1174e33dd',
+  apiUrl: `https://pixabay.com/api`
 };
 
 /*
